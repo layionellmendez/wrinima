1dffc7f06b7be74ee288d03b60926aae4b54e8b7
Layionell Stwart Méndez Jiménez <67165194+layionellmendez@users.noreply.github.com>
2023-07-13 04:37:17 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const cacheName = 'wrinima-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/site.webmanifest',
+  '/favicon.ico',
+  '/android-chrome-192x192.png',
+  '/android-chrome-512x512.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
