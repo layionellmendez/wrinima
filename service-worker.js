736fff0b2ecb4a46530689b7f087d8d8058e934e
Layionell Stwart Méndez Jiménez <67165194+layionellmendez@users.noreply.github.com>
2023-07-13 13:23:26 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
-const cacheName = 'wrinima-cache-v1';
-const filesToCache = [
-  'index.html',
-  'site.webmanifest',
-  'favicon.ico',
-  'android-chrome-192x192.png',
-  'android-chrome-512x512.png',
-];
-
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll(filesToCache);
+    caches.open('wrinima-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        'index.html',
+        'manifest.json',
+        'android-chrome-192x192.png',
+        'android-chrome-512x512.png',
+        'apple-touch-icon.png',
+        'favicon.ico'
+        /* Agrega aquí cualquier otro recurso que desees cachear */
+      ]);
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
     })
   );
